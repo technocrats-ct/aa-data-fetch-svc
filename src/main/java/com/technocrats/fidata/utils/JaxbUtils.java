98381f917da22407db552a9056e555205d0dd57a
GoluKumar4024 <str.golu@gmail.com>
2023-02-15 13:23:52 +0530

    initial commit

--- src/main/java/com/technocrats/fidata/utils/JaxbUtils.java
+++ src/main/java/com/technocrats/fidata/utils/JaxbUtils.java
@@ -1,6 +1,35 @@
 package com.technocrats.fidata.utils;
 
+
+import com.technocrats.fidata.types.Account;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
+import java.io.StringReader;
+
 public class JaxbUtils {
 
+//    public static void marshal(Account,String xmlStr)
+//            throws IOException, JAXBException {
+//        JAXBContext context;
+//        BufferedWriter writer = null;
+//        writer = new BufferedWriter(new FileWriter(selectedFile));
+//        context = JAXBContext.newInstance(Employee.class);
+//        Marshaller m = context.createMarshaller();
+//        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+//        m.marshal(employee, writer);
+//        writer.close();
+//    }
 
+    // Import: Unmarshalling
+    public static Account unmarshal(String xmlStr) throws JAXBException {
+        Account account = null;
+        JAXBContext context;
+        context = JAXBContext.newInstance(Account.class);
+        Unmarshaller um = context.createUnmarshaller();
+        account = (Account) um.unmarshal(new StringReader(xmlStr));
+        return account;
+    }
 }
